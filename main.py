9833b99814f649f660c27a23cfcbf09319950732
Utaemon-Mad <danielpgo87@gmail.com>
2026-07-06 22:09:45 +0200

    pruebas main codigo depuracion

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print('hola mundo')
-print('este es un ejemplo de un cambio en el archivo main.py')
+nombre = 'Enzo'
+edad = 0
+alergias = ['Coco']
+mensaje = f'Hola, mi nombre es {nombre} tengo {edad} años y soy alérgico a {alergias[0]}'
+print(mensaje)
