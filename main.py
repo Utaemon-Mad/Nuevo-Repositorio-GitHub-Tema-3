31753df211d767ff368fc476d53a90d07a8a658e
Utaemon-Mad <danielpgo87@gmail.com>
2026-07-12 22:39:46 +0200

    prueba Datos Medicos Enzo

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 nombre = 'Enzo'
 edad = 0
 alergias = ['Coco']
+if edad < 18:
+    print('Eres mayor de edad')
+if alergias and alergias[0] == 'Coco':
+    print('pobrecito')
 mensaje = f'Hola, mi nombre es {nombre} tengo {edad} años y soy alérgico a {alergias[0]}'
 print(mensaje)
