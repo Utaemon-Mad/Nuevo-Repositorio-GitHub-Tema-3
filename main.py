e3793f49565e62f3365aef8f2b71dd35251eea20
Utaemon-Mad <danielpgo87@gmail.com>
2026-07-21 22:43:38 +0200

    Tabla de colores

--- main.py
+++ main.py
@@ -7,3 +7,29 @@ if alergias and alergias[0] == 'Coco':
     print('pobrecito')
 mensaje = f'Hola, mi nombre es {nombre} tengo {edad} años y soy alérgico a {alergias[0]}'
 print(mensaje)
+
+
+
+
+saldo = 1600
+coche = 210
+
+if saldo >= coche:
+    print('Te fundes el sueldo en el coche')
+
+
+
+
+
+
+color = input('Introduce un color: ')
+if color == 'rojo':
+    print('El color es rojo')
+elif color == 'verde':
+    print('El color es verde')
+elif color == 'azul':
+    print('El color es azul')
+else: 
+    print('Eres mas daltonico que un culo')
+
+    
